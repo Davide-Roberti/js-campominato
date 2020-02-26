9fd532d9cf37f65375aaea320dfcdde799f47959
Davide <davide.roberti89@gmail.com>
2020-02-26 15:09:37 +0100

    creazione 16 numeri casuali racchiuso in una funzione

--- js/main.js
+++ js/main.js
@@ -4,22 +4,14 @@
 // chiedo all'utente di inserire dei numeri tramite un prompt in un ciclo while
 // se il numero è presente nell'array di numeri generati il gioco finisce, altrimenti chiedo all'utente un'altro numero
 
-var arrayNumeriBomba = [];
-var arrayNumeriInseriti = [];
 var difficolta = cambiaDifficolta();
-
-
-while (arrayNumeriBomba.length < 16) {
-    var numeroBomba = generaRandom(1, difficolta);
-    if (!arrayNumeriBomba.includes(numeroBomba)){
-    arrayNumeriBomba.push(numeroBomba)
-    }
-}
+var campoMinato = posizioneMine(difficolta);
+var arrayNumeriInseriti = [];
 
 var sentinella = false;
-while ((arrayNumeriInseriti.length < difficolta - arrayNumeriBomba.length) && (sentinella == false)) {
+while ((arrayNumeriInseriti.length < difficolta - campoMinato.length) && (sentinella == false)) {
     var numeroInserito = parseInt(prompt('inserisci un numero tra 1 e ' + difficolta));
-    if (arrayNumeriBomba.includes(numeroInserito)) {
+    if (campoMinato.includes(numeroInserito)) {
         alert ('BOOOOOOOOOM!')
         sentinella = true;
     } else if (arrayNumeriInseriti.includes(numeroInserito)) {
@@ -30,7 +22,7 @@ while ((arrayNumeriInseriti.length < difficolta - arrayNumeriBomba.length) && (s
 }
 
 console.log('sei sopravvissuto per un totale di ' + arrayNumeriInseriti.length + ' mosse, sei ESPLOSO alla mossa numero ' + (arrayNumeriInseriti.length + 1));
-console.log('i numeri BOMBA erano: ' + arrayNumeriBomba);
+console.log('i numeri BOMBA erano: ' + campoMinato);
 
 function cambiaDifficolta () {
     var difficoltaScelta = parseInt(prompt('scegli un livello di difficoltà: 0 = facile, 1 = medio, 2 = difficile'))
@@ -49,6 +41,18 @@ function cambiaDifficolta () {
     }
     return possibilita;
 }
+
+function posizioneMine (valDifficolta) {
+    var arrayNumeriBomba = [];
+    while (arrayNumeriBomba.length < 16) {
+        var numeroBomba = generaRandom(1, valDifficolta);
+        if (!arrayNumeriBomba.includes(numeroBomba)){
+        arrayNumeriBomba.push(numeroBomba)
+        }
+    }
+    return arrayNumeriBomba;
+}
+
 
 function generaRandom(min, max) {
     numeroRandom = Math.floor(Math.random() * (max - min + 1)) + min;
